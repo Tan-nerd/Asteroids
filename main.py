7d3ab163ad9d64768dc7b0b99cbb24f203895101
Tannerd <tanman7210@gmail.com>
2025-04-13 17:47:09 -0400

    add player, update main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
+from constants import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
 
@@ -7,12 +9,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, 0)
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
 
