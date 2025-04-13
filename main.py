c4c280c49c6aa5d53f262eb4ca0f0ad0c7e8cbf7
Tannerd <tanman7210@gmail.com>
2025-04-13 17:07:16 -0400

    add screen, gameloop, gitignore

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MAX_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_SPAWN_RATE
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, 0)
+        pygame.display.flip()
+
+    """ print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}") """
 
 if __name__ == "__main__":
     main()
